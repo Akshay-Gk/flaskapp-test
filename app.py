3f091d9e67991327d1e2ef2a62acaabf3b4e23d5
Akshay G Krishnan <112197849+Akshay-Gk@users.noreply.github.com>
2023-09-11 22:46:09 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import os
+ 
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1><center>Flask application version1</center></h1>'
+ 
+flask_port = os.getenv('FLASK_PORT',8080)
+app.run(host='0.0.0.0', port=flask_port)
