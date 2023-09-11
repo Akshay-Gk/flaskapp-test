4ec0096bc3127929c10bd94bea96f52cec78c358
Akshay G Krishnan <112197849+Akshay-Gk@users.noreply.github.com>
2023-09-11 23:39:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return '<h1><center>Flask application version3</center></h1>'
+    return '<h1><center>Flask application version4</center></h1>'
  
 flask_port = os.getenv('FLASK_PORT',8080)
 app.run(host='0.0.0.0', port=flask_port)
